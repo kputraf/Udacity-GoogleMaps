c931689293599ae9a31d72763a4a9e209cfd8095
kputraf <contact.krisnapf@gmail.com>
2017-11-23 20:28:05 +0700

    Change location with button

--- app/src/main/java/com/example/krisnaputraf/myapplication/MainActivity.java
+++ app/src/main/java/com/example/krisnaputraf/myapplication/MainActivity.java
@@ -1,13 +1,85 @@
-package com.example.krisnaputraf.myapplication;
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    static final CameraPosition NEWYORK = CameraPosition.builder()
+            .target(new LatLng(40.784, -73.9857))
+            .zoom(17)
+            .bearing(0)
+            .build();
+     static final CameraPosition SEATTLE = CameraPosition.builder()
+             .target(new LatLng(47.6204, -122.3491))
+             .zoom(17)
+             .bearing(0)
+             .build();
+     static final CameraPosition DUBLIN = CameraPosition.builder()
+             .target(new LatLng(53.3478, -6.2597))
+             .zoom(17)
+             .bearing(90)
+             .build();
+     static final CameraPosition TOKYO = CameraPosition.builder()
+             .target(new LatLng(35.6895, 139.6917))
+             .zoom(17)
+             .bearing(90)
+             .build();
+     GoogleMap m_map;
+     boolean mapReady = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnSeattle = (Button) findViewById(R.id.btnSeattle);
+        btnSeattle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mapReady)
+                    flyTo(SEATTLE);
+            }
+        });
+
+        Button btnDublin = (Button) findViewById(R.id.btnDublin);
+        btnDublin.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 if (mapReady)
+                     flyTo(DUBLIN);
+             }
+        });
+
+        Button btnTokyo = (Button) findViewById(R.id.btnTokyo);
+        btnTokyo.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 if (mapReady)
+                     flyTo(TOKYO);
+             }
+        });
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mapReady = true;
+        m_map = googleMap;
+        m_map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        flyTo(NEWYORK);
+    }
+
+    private void flyTo(CameraPosition target) {
+        m_map.animateCamera(CameraUpdateFactory.newCameraPosition(target), 10000, null);
     }
 }
